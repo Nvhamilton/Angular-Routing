7daa3e6dfef6f71e0f5aa8db107a0e9aa4f58ae8
Nvhamilton <60369628+Nvhamilton@users.noreply.github.com>
2021-06-24 20:28:58 -0500

    route path update

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';//imported for tutorial 
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ProductData } from './products/product-data';
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+/* Feature Modules */
+import { ProductModule } from './products/product.module';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './messages/message.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,//basic directives
+    HttpClientModule,//basic http directibes
+    InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),//simulates APi calls
+    ProductModule,//feature module
+    UserModule,//feature module
+    MessageModule,//feature module
+    RouterModule.forRoot([
+      {path: 'welcome', component:WelcomeComponent},
+      {path:'', redirectTo:'welcome', pathMatch:'full'},//blank paths redirects to the welcome route, full path match
+      {path:'**', component:PageNotFoundComponent}
+    ])//modules get imported so the app may recognize it's existence..takes an array of objects
+  ],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    PageNotFoundComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
